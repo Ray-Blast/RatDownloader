bd4244b40c8cb5b31e6fc022465bfc190a5a2882
Ray-Blast <rayblast179@gmail.com>
2024-06-13 03:37:16 -0500

    Fixed some small bugs + now we have a working GUI + DL interface

--- RatGUI.py
+++ RatGUI.py
@@ -6,19 +6,37 @@ from tkinter import ttk
 user_url = "https://www.youtube.com/watch?v=_m9LHQjsGYM"
 user_path = "downloads"
 
-def download_vid_button(vid_url, path):
-    dl.downloadVideo(vid_url, path)
-
 
 class App(tk.Frame):
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.label = tk.Label(self.frame, text="Download Video")
-        self.label.pack()
-        self.Download_Button = tk.Button(self.frame, text="Download", command=lambda: download_vid_button(user_url, user_path))
+        self.title_label = tk.Label(self.frame, text="Download Video\n\n")
+        self.title_label.pack()
+
+        self.url = tk.StringVar()
+        self.path = tk.StringVar()
+
+        self.url_label = tk.Label(self.frame, text="URL:")
+        self.url_label.pack()
+        self.url_entry = tk.Entry(self.frame, textvariable=self.url)
+        self.url_entry.pack()
+
+        self.path_label = tk.Label(self.frame, text="Path:")
+        self.path_label.pack()
+        self.path_entry = tk.Entry(self.frame, textvariable=self.path)
+        self.path_entry.pack()
+
+        self.Download_Button = tk.Button(self.frame, text="Download", command=lambda: self.download_vid_button())
         self.Download_Button.pack()
         self.frame.pack()
+
+    def download_vid_button(self):
+        vid_url = self.url.get()
+        vid_path = self.path.get()
+
+        dl.downloadVideo(vid_url, vid_path)
+        self.url.set("")
 
 
 def run_app():
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # Rat DOwnloader
 # Created by Ray Blast
 
-import downloader as dl
 import RatGUI
 
 if __name__ == '__main__':
     RatGUI.run_app()
-    # user_url = input("Please enter the url you want to download: ")
-    # dl.downloadVideo(user_url, download_directory)
